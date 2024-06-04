585e5fedd76f18f22b71262f80722c584d243d65
WilJCPimenta <wiljcpimenta@gmail.com>
2024-06-04 20:58:02 -0300

    Modelando categorias

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -0,0 +1,28 @@
+package br.com.alura.screenmatch.model;
+
+public enum Categoria {
+    ACAO("Action"),
+
+    ROMANCE("Romance"),
+
+    COMEDIA("Comedy"),
+
+    DRAMA("Drama"),
+
+    CRIME("Crime");
+
+    private String categoriaOmdb;
+    Categoria(String getCategoriaOmdb){
+        this.categoriaOmdb = getCategoriaOmdb;
+    }
+
+    public static Categoria fromString(String text){
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenuma categoria encontrada para a string");
+        }
+    }
+
--- screenmatch/src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -1,10 +1,9 @@
 package br.com.alura.screenmatch.service;
 
-import br.com.alura.screenmatch.model.DadosSerie;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class ConverterDados implements IConverteDados{
+public class ConverteDados implements IConverteDados{
     private ObjectMapper mapper = new ObjectMapper();
 
     @Override
