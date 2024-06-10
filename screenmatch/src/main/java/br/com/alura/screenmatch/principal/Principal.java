5dc9b9185ebf39ffe0f3f30bba48e8ec41af3aa6
WilJCPimenta <wiljcpimenta@gmail.com>
2024-06-10 00:44:21 -0300

    JPQL: persistindo dados

--- screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -48,7 +48,8 @@ public class Principal {
                     0 - Sair                                 
                     """;
 
-            System.out.println(menu);
+            System.out.println(menu
+            );
             opcao = leitura.nextInt();
             leitura.nextLine();
 
